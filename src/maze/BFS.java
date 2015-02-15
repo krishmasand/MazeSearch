b3239abf998e6629e8185e7f5b5b4ac40a9853eb
nitropolo13 <rodneyshag.com@gmail.com>
2015-02-15 16:21:33 -0600

    BFS
    just starting

---
+++ src/maze/BFS.java
@@ -0,0 +1,15 @@
+package maze;
+import maze.*;
+
+public class BFS {
+	//Maze maze = new Maze("smallMaze.txt");
+	/* Declare Data */
+	
+	/* Initialize Data */
+	
+	/* Load 1st Vertex */
+	while(!myQueue.empty())
+	{
+		
+	}
+}
