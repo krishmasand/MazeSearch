19ab3b9e1281791868a866a652946579f46e96aa
nitropolo13 <rodneyshag.com@gmail.com>
2015-02-15 18:01:01 -0600

    BFS Implemented

--- src/maze/BFS.java
+++ src/maze/BFS.java
@@ -1,15 +1,50 @@
 package maze;
-import maze.*;
+import java.util.*;
 
 public class BFS {
-	//Maze maze = new Maze("smallMaze.txt");
-	/* Declare Data */
+
+	/* Data */
+	public Queue<Point> frontier;
+	public Vector<Point> visited;
+	Map<Point, Point> predecessor = new HashMap<Point, Point>();
+	public int solutionDistance;
 	
-	/* Initialize Data */
+	/* Constructor */
+	public BFS(Maze maze){
+		Point endPoint = findSolution(maze);
+		solutionDistance = getSolutionsDistance(endPoint);
+	}
 	
-	/* Load 1st Vertex */
-	while(!myQueue.empty())
-	{
+	public Point findSolution(Maze maze){
+		/* Load Start Point onto Frontier */
+		frontier.add(maze.start);
+		visited.add(maze.start);
 		
+		/* Actual Algorithm */
+		while( ! frontier.isEmpty())
+		{
+			Point currentPoint = frontier.remove();
+			Vector<Point> adjacentPoints = currentPoint.getAdjacentPoints(maze);
+			for(Point point : adjacentPoints){
+				if ((point.pointType == PointType.EMPTY || point.pointType == PointType.DOT) && !visited.contains(point)){
+					predecessor.put(point, currentPoint);
+					frontier.add(point);
+					visited.add(point);
+					if (point == maze.end)
+						return point;
+				}
+			}	
+		}
+		return null;
+	}
+	
+	/* Assumes the Hashmap "predecessor" has already been updated */
+	public int getSolutionsDistance(Point currentPoint){
+		int distance = 0;
+		while(predecessor.containsKey(currentPoint)) {
+			currentPoint = predecessor.get(currentPoint);
+			distance++;
+		}
+		return distance;
 	}
 }
--- src/maze/Point.java
+++ src/maze/Point.java
@@ -1,6 +1,7 @@
 package maze;
 
 import maze.*;
+import java.util.Vector;
 
 public class Point {
 	/* Data */
@@ -9,6 +10,13 @@ public class Point {
 	PointType pointType;
 	
 	/* Standard Constructor */
+	public Point(int xCoordinate, int yCoordinate){
+		x = xCoordinate;
+		y = yCoordinate;
+		pointType = null;
+	}
+	
+	/* May not need this */
 	public Point(int xCoordinate, int yCoordinate, PointType type){
 		x = xCoordinate;
 		y = yCoordinate;
@@ -22,6 +30,23 @@ public class Point {
 		pointType = otherPoint.pointType;
 	}
 	
+	/* Returns: WALLS, DOTS, and EMPTY spaces */
+	public Vector<Point> getAdjacentPoints(Maze maze){
+		Vector<Point> points = new Vector<Point>();
+		
+		/* Find up to 4 valid (not off the map) Positions */
+		if(maze.validPosition(maze.grid[x+1][y]))
+			points.add(maze.grid[x+1][y]);
+		if(maze.validPosition(maze.grid[x-1][y]))
+			points.add(maze.grid[x-1][y]);
+		if(maze.validPosition(maze.grid[x][y+1]))
+			points.add(maze.grid[x][y+1]);
+		if(maze.validPosition(maze.grid[x][y-1]))
+			points.add(maze.grid[x][y-1]);
+
+		return points;
+		
+	}
 	// Overriding equals() to compare two objects
     @Override
     public boolean equals(Object obj) {
