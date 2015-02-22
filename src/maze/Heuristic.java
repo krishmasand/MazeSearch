e835f00e6efa97fc88f6945beb5c8053bc9685a6
nitropolo13 <rodneyshag.com@gmail.com>
2015-02-21 18:34:40 -0600

    Added another crappy heuristic 1.3

--- src/maze/Heuristic.java
+++ src/maze/Heuristic.java
@@ -4,7 +4,6 @@ package maze;
 Heuristic object that calculates Heuristics for A* Search and Greedy Best First Search
  */
 
-import java.lang.*;
 
 public class Heuristic {
 
@@ -26,17 +25,44 @@ public class Heuristic {
 
     public int pacmanHeuristic(Point point, Maze maze) {
         int farthestPoint = 0;
+        int numDotsLeft = maze.dotCount;
+        
         for(int i = 0; i < maze.columns; i++) {
             for(int j = 0; j < maze.rows; j++) {
                 farthestPoint = Math.max(maze.grid[i][j].heuristic.distanceHeuristic, farthestPoint);
             }
         }
-        int numDotsleft = maze.dotCount;
-
-
-
-
-        return 0;
+        
+        Point firstPoint;
+        Point secondPoint;
+        int twoPointDistance = 0;
+        int currentDistance = 0; // temporary variable
+        
+        /* Loop Through Maze for 1st Point */
+        for(int i = 0; i < maze.columns; i++){
+            for(int j = 0; j < maze.rows; j++){
+            	if(maze.grid[i][j].pointType == PointType.DOT){
+            		firstPoint = maze.grid[i][j];
+            		
+            		/* Loop through maze again to find distances for firstPoint to secondPoint */
+            		for (int m = 0; m < maze.columns; m++){
+            			for (int n = 0; n < maze.rows; n++){
+            				if (maze.grid[m][n].pointType == PointType.DOT){
+            					secondPoint = maze.grid[m][n];
+            					 currentDistance = manhattanDistance(firstPoint, secondPoint);
+            					 if (currentDistance > twoPointDistance)
+            						 twoPointDistance = currentDistance;
+            				}
+            			}
+            		}
+            	}
+            }
+        }
+        
+        return Math.max(Math.max(farthestPoint, numDotsLeft), twoPointDistance);
     }
-
+    
+    public int manhattanDistance(Point firstPoint, Point secondPoint){
+    	return Math.abs(firstPoint.x - secondPoint.x) + Math.abs(firstPoint.y - secondPoint.y);
+    }
 }
