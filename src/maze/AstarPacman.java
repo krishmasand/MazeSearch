b3c68d9661e11de3b279c416b848403086aad88d
nitropolo13 <rodneyshag.com@gmail.com>
2015-02-22 15:05:13 -0600

    fixed AstarPacman to match Astar

--- src/maze/AstarPacman.java
+++ src/maze/AstarPacman.java
@@ -32,8 +32,8 @@ public class AstarPacman {
         while( ! frontier.isEmpty())
         {
             Point currentPoint = frontier.remove();
+            setAdjacentHeuristics(maze, currentPoint);
             nodesExpanded++;
-            setHeuristics(maze);
             Vector<Point> adjacentPoints = currentPoint.getAdjacentPoints(maze);
 		
 			/* Loop through adjacent points and update stuff */
@@ -73,6 +73,15 @@ public class AstarPacman {
                 if(maze.grid[i][j].pointType == PointType.EMPTY || maze.grid[i][j].pointType == PointType.DOT) {
                     maze.grid[i][j].heuristic = new Heuristic(maze.grid[i][j], maze);
                 }
+            }
+        }
+    }
+    
+    public void setAdjacentHeuristics(Maze maze, Point popped) {
+        for(Point point : popped.getAdjacentPoints(maze)) {
+            if ((point.pointType == PointType.EMPTY || point.pointType == PointType.DOT) && !visited.contains(point)){
+                point.heuristic = new Heuristic(point, maze);
+                point.heuristic.astarHeuristic += getSolution(maze, popped);
             }
         }
     }
