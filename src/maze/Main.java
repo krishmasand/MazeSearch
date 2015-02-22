fe092a668f22a77d1507122b21b34c616fb8459a
nitropolo13 <rodneyshag.com@gmail.com>
2015-02-22 14:22:43 -0600

    Changed MAIN. Put new txt files in correct place.

--- src/maze/Main.java
+++ src/maze/Main.java
@@ -5,34 +5,30 @@ import java.io.IOException;
 public class Main {
 	
 	public static void main(String [] args) throws IOException{
+        /*******/
+        /* 1.1 */
+        /*******/
 		
 		/* Create Mazes */
 		Maze maze1 = new Maze("smallMaze");
 		Maze maze2 = new Maze("smallMaze");
 		Maze maze3 = new Maze("smallMaze");
         Maze maze4 = new Maze("smallMaze");
-		
-
-		//BFS solution = new BFS(maze1);
+        
         BFS solution = new BFS(maze1);
         DFS solution2 = new DFS(maze2);
         Greedy solution3 = new Greedy(maze3);
         Astar solution4 = new Astar(maze4);
-        //Greedy solution2 = new Greedy(maze2);
-        //Greedy solution3 = new Greedy(maze3);
-
-        //BFS solution = new BFS(maze1);
-		//System.out.println(solution.solutionDistance);
-		
+	
 		/* Print Mazes */
         System.out.println("Breadth First Search");
-		System.out.print(maze1);
+		System.out.println(maze1);
         System.out.println("Depth First Search");
-        System.out.print(maze2);
+        System.out.println(maze2);
         System.out.println("Greedy Best First Search");
-		System.out.print(maze3);
+		System.out.println(maze3);
         System.out.println("A* Search");
-        System.out.print(maze4);
+        System.out.println(maze4);
 				
 		System.out.println("BFS Nodes Expanded = " + solution.nodesExpanded);
         System.out.println("DFS Nodes Expanded = " + solution2.nodesExpanded);
@@ -43,52 +39,50 @@ public class Main {
         System.out.println("DFS Solution Distance = " + solution2.solutionDistance);
         System.out.println("Greedy Best-First Solution Distance = " + solution3.solutionDistance);
         System.out.println("A* Solution Distance = " + solution4.solutionDistance);
-
-//		//Print Small Maze
-//		for(int j = 0; j < m.rows; j++){
-//			for(int i = 0; i < m.columns; i++){
-//				System.out.print(m.grid[i][j]);
-//			}
-//			System.out.println("");
-//		}
-//		
-//		
-//		Maze m2 = new Maze("mediumMaze");
-//		
-//		
-//		//Print Medium Maze
-//		for(int j = 0; j < m2.rows; j++){
-//			for(int i = 0; i < m2.columns; i++){
-//				System.out.print(m2.grid[i][j]);
-//			}
-//			System.out.println("");
-//		}
-		
-//		String everything = "hi";
-//		
-//		File file = new File("smallMaze.txt");
-//		if ( file.exists() )
-//		    System.out.println( "Exists" );
-//
-//		BufferedReader br = new BufferedReader(new FileReader("smallMaze.txt"));
-//	    try {
-//	        StringBuilder sb = new StringBuilder();
-//	        String line = br.readLine();
-//	
-//	        while (line != null) {
-//	            sb.append(line);
-//	            sb.append(System.lineSeparator());
-//	            line = br.readLine();
-//	        }
-//	        everything = sb.toString();
-//	    } finally {
-//	        br.close();
-//	    }
-//	    
-//	    
-//	   System.out.println(everything);
-	    
-	}
+        System.out.println();
 	
-	
+        /*******/
+        /* 1.2 */
+        /*******/
+        
+        /* Greedy */
+        Maze maze_5_greedy = new Maze("goodForAStar");
+        Greedy maze_5_greedySolution = new Greedy(maze_5_greedy);
+ 
+        System.out.println("Special Maze: Should be bad for Greedy Search");
+        System.out.print(maze_5_greedy);
+        System.out.println("Nodes Expanded = " + maze_5_greedySolution.nodesExpanded);
+        System.out.println("Solution Distance = " + maze_5_greedySolution.solutionDistance);
+        System.out.println();
+        
+        /* A-star */
+        Maze maze_5_astar = new Maze("goodForAStar");
+        Astar maze_5_astarSolution = new Astar(maze_5_astar);
+ 
+        System.out.println("Special Maze: Should be good for Astar Search");
+        System.out.println(maze_5_astar);
+        System.out.println("Nodes Expanded = " + maze_5_astarSolution.nodesExpanded);
+        System.out.println("Solution Distance = " + maze_5_astarSolution.solutionDistance);
+        System.out.println();
+        
+        /* Greedy */
+    /*    Maze maze_6_greedy = new Maze("goodForGreedy");
+        Greedy maze_6_greedySolution = new Greedy(maze_6_greedy);
+ 
+        System.out.println("Special Maze: Should be bad for Greedy Search");
+        System.out.print(maze_6_greedy);
+        System.out.println("Nodes Expanded = " + maze_6_greedySolution.nodesExpanded);
+        System.out.println("Solution Distance = " + maze_6_greedySolution.solutionDistance);
+        System.out.println();*/
+        
+        /* A-star */
+     /*   Maze maze_6_astar = new Maze("goodForGreedy");
+        Astar maze_6_astarSolution = new Astar(maze_6_astar);
+ 
+        System.out.println("Special Maze: Should be good for Astar Search");
+        System.out.println(maze_6_astar);
+        System.out.println("Nodes Expanded = " + maze_6_astarSolution.nodesExpanded);
+        System.out.println("Solution Distance = " + maze_6_astarSolution.solutionDistance);
+        System.out.println();*/
+	}	
 }
