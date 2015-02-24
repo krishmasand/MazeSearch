32048754f23e379e3b2e226a341b0bbb06dc145a
Perilynn <vishvind@gmail.com>
2015-02-23 21:27:42 -0600

    Improved heuristics for A* Search

--- src/maze/StateHeuristic.java
+++ src/maze/StateHeuristic.java
@@ -10,11 +10,19 @@ public class StateHeuristic {
 	public void updateStateHeuristic(State state){
 		int numDotsLeft_Heuristic = state.dots.size();
 		int closestDot_Heuristic = Integer.MAX_VALUE;
-		
+        closestDot_Heuristic = findClosestDot(state);
 		heuristic = closestDot_Heuristic + (numDotsLeft_Heuristic - 1);
 	}
 	
 	private int manhattanDistance(Point firstPoint, Point secondPoint){
         return Math.abs(firstPoint.x - secondPoint.x) + Math.abs(firstPoint.y - secondPoint.y);
+    }
+
+    private int findClosestDot(State state) {
+        int temp = Integer.MAX_VALUE;
+        for(Point point: state.dots) {
+            temp = Math.min(manhattanDistance(point, state.pacmanLocation), temp);
+        }
+        return temp;
     }
 }
