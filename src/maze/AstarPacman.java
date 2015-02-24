72a23438624b085656aa24a430d27f52f6c0a419
nitropolo13 <rodneyshag.com@gmail.com>
2015-02-23 22:57:50 -0600

    Code is pretty solid

--- src/maze/AstarPacman.java
+++ src/maze/AstarPacman.java
@@ -44,6 +44,9 @@ public class AstarPacman {
         }
         return null;
     }
+    public int getSolution(Maze maze, State state){    	
+    	return state.costSoFar;
+    }
 }
 
 
--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -10,7 +10,7 @@ public class Maze {
 
 	Point start;
 	Point end;
-
+	
     /* When scanning the maze, count up the number of dots that are in the maze (for 1.3 only) */
     int dotCount = 0;
 
